96e21686b547ba36a03aa7c334d25f785a21d0b1
cehdly99 <mohamed.chedliiy@gmail.com>
2021-05-03 16:31:13 +0100

    gather data

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Retrieve the user and the corresponding done tasks
+"""
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    userId = argv[1]
+    task_titel = []
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                        format(userId)).json()
+    tsks = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                        format(userId)).json()
+    for task in tsks:
+        if task.get('completed') is True:
+            task_titel.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}):".
+          format(user.get('name'), len(task_titel), len(tsks)))
+    for i in task_titel:
+        print("\t {}".format(i))
