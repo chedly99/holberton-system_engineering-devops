12a82d63393e265f9919982a3bd32a4c3cf77682
cehdly99 <mohamed.chedliiy@gmail.com>
2021-05-18 22:23:58 +0100

    task2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+hot articles for a given subreddit.
+"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], after_party=None):
+    header = {'User-Agent': 'fake'}
+    param = {'after': after_party}
+    r = requests.get('http://www.reddit.com/r/{}/hot.json?after={}'
+                     .format(subreddit, after_party),
+                     headers=header)
+    status = r.status_code
+    if status == requests.codes.ok:
+        x = r.json().get('data').get('children')
+        after_party = r.json().get('data').get('after')
+        for page in x:
+            hot_list.append(page.get('data').get('title'))
+        if len(hot_list) == 0:
+            return None
+        if after_party is None:
+            return hot_list
+        return recurse(subreddit, hot_list, after_party)
+    else:
+        return None
